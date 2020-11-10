91ef867c23499f0b57eda1cc501158b2665411ac
Mario <marioarbesu85@gmail.com>
2020-11-10 22:59:40 +0100

    Fix npm start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', require('./routes/index'));
 
-app.listen(5000, () => console.log("listening on port 5000"));
+app.listen(PORT, () => console.log(`listening on port ${ PORT }`));
